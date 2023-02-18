017f3ad2eff48b8368687394a6ad049eee3ae0cf
fabianjh11 <fama11u3@gmail.com>
2023-02-17 19:18:03 -0600

    second commit

---
+++ public/chat.js
@@ -0,0 +1,62 @@
+//Connection
+
+var socket = io.connect('http://localhost');
+
+const handle = document.getElementById('handle');
+const message = document.getElementById('message');
+const btn = document.getElementById('send');
+const output = document.getElementById('output');
+const feedback = document.getElementById('feedback');
+
+// Emitir eventos
+btn.addEventListener('click', function()
+{
+  if(message.value !== '')
+  {
+    if(handle.value === '')
+    {
+      handle.value = 'User';
+    }
+    socket.emit('chat', {
+      message: message.value,
+      handle: handle.value
+    });
+  }
+});
+
+document.addEventListener('keypress', function(keypressed)
+{
+  if(keypressed.key === 'Enter' && message.value !== '')
+  {
+    if(handle.value === '')
+    {
+      handle.value = 'User';
+    }
+    socket.emit('chat', {
+      message: message.value,
+      handle: handle.value
+    });
+  }
+});
+
+message.addEventListener('keypress', function()
+{
+  if(handle.value === '')
+  {
+    handle.value = 'User';
+  }
+  socket.emit('typing', handle.value);
+});
+
+//Escuchar eventos y emitir el mensaje a todos los clientes
+socket.on('chat', function(data)
+{
+  feedback.innerHTML = "";
+  output.innerHTML += '<p><strong>' + data.handle + ':</strong> ' + data.message + '</p>';
+  message.value = '';
+})
+
+socket.on('typing', function(data)
+{
+  feedback.innerHTML = '<p><em> ' + data + ' está escribiendo...</em></p>';
+});
